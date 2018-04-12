712404e295e44ba054b042ea4024e334149b225d
Kishore7298 <35829521+Kishore7298@users.noreply.github.com>
2018-04-12 06:39:59 +0530

    Final one

--- 2.EdgeOfTheOccean/Program5/mySolution.js
+++ 2.EdgeOfTheOccean/Program5/mySolution.js
@@ -1,8 +1,8 @@
 function matrixElementsSum(matrix) {
     var i,j,p,k,l;
     var sum=0;
-for(i=0;i<=3;i++)
-    for(j=0;j<=4;j++)
+for(i=0;i<matrix.length;i++)
+    for(j=0;j<matrix[0].length;j++)
         {
             if(matrix[i][j]== 0)
              for(p=i;p<3;p++)
@@ -10,8 +10,8 @@ for(i=0;i<=3;i++)
                      matrix[p][j] = 0;
                  }
         }
-    for(k=0;k<3;k++)
-        for(l=0;l<4;l++)
+    for(k=0;k<matrix.length;k++)
+        for(l=0;l<matrix[0].length;l++)
             sum=sum+matrix[k][l];
     return sum;
         
